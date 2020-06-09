b21c71f21da15bee50e6433bf3956e04788003af
Erica Cannedy <encannedy@gmail.com>
2020-06-08 23:52:12 -0400

    updates to functions

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-//npm init
-//npm install - my sql, inquirer
-//mysql connection/connect
-
 var mysql= require("mysql");
 var inquirer = require("inquirer");
 var connection = mysql.createConnection({
@@ -11,7 +7,7 @@ port: "3306",
 
 user: "root",
 
-passsword: "password",
+password: "password",
 database: "Employee_Tracker_db"
 });
 
@@ -27,21 +23,24 @@ function start(){
     .prompt({
         name: "menu",
         type: "list",
-        message: "Would you like to view [VIEW DEPARTMENTS], [VIEW ROLES], [VIEW EMPLOYEES], [ADD DEPARTMENT], [ADD ROLES]. [ADD EMPLOYEES], [EXIT]?",
-        choices: ["VIEW_DEPARTMENTS", "VIEW_ROLES", "VIEW_EMPLOYEES", "EXIT"],
+        message: "Would you like to view [VIEW DEPARTMENT], [VIEW ROLE], [VIEW EMPLOYEE], [ADD DEPARTMENT], [ADD ROLE], [ADD EMPLOYEE],[UPDATE EMPLOYEE] [EXIT]?",
+        choices: ["VIEW_DEPARTMENT", "VIEW_ROLE", "VIEW_EMPLOYEE", "ADD_DEPARTMENT", "ADD_ROLE", "ADD_EMPLOYEE", "UPDATE_EMPLOYEE", "EXIT"]
     }).then(function(answer) {
-        if (answer.menu ==="VIEW_DEPARTMENTS") {
+        if (answer.menu ==="VIEW_DEPARTMENT") {
             viewDepartments();
-        }else if (answer.menu === "VIEW_ROLES") {
+        }else if (answer.menu === "VIEW_ROLE") {
             viewRoles();
-        }else if (answer.menu === "VIEW_EMPLOYEES") {
+        }else if (answer.menu === "VIEW_EMPLOYEE") {
             viewEmployees();
-        }        if (answer.menu ==="ADD_DEPARTMENTS") {
+        }        if (answer.menu ==="ADD_DEPARTMENT") {
             addDepartment();
-        }else if (answer.menu === "ADD_ROLES") {
+        }else if (answer.menu === "ADD_ROLE") {
             addRoles();
-        }else if (answer.menu === "ADD_EMPLOYEES") {
+        }else if (answer.menu === "ADD_EMPLOYEE") {
             addEmployees();
+        }else if (answer.menu === "UPDATE_EMPLOYEE") {
+            updateEmployees();
+       
         }else {
             // exit - close connection
             connection.end();
@@ -58,8 +57,8 @@ function viewDepartments(){
 }
 //view roles- selct * from departments
 function viewRoles(){
-    connection.query(`select title, salary, name from rolles 
-    inner join department on roles.department_id=department.id`, function(err, results) {
+    connection.query(`select title, salary, name from role 
+    inner join department on role.department_id=department.id`, function(err, results) {
         if (err) throw err;
         console.table(results);
         start();
@@ -68,18 +67,118 @@ function viewRoles(){
 //view employess - select * from employee
 function viewEmployees(){
     connection.query(`select first_name, last_name, title, salary, name from employee 
-    inner join roles on employee.role_id=roles.id
-    inner join department on roles.department_id=department.id`, function(err, results) {
+    inner join role on employee.role_id=role.id
+    inner join department on role.department_id=department.id`, function(err, results) {
         if (err) throw err;
         console.table(results);
         start();
     })
 };
 
-//selct title, slary, name from role
-//ninner join department on role.deaprtment_id=department.id;
-
-//selct first_nae. last_name, titile, salary, name from employee
+function printResults (err, result) {
+    if (err) throw err;
+    console.log(result.affectedRows + " deleted.");
+    start();
+}
+async function addDepartment () {
+    const department = await inquirer.prompt([
+        {
+            name: "name",
+            message: "What is the name of the department"
+        }
+    ])
+    connection.query (`insert into department (name) values ('${department.name}')`, printResults )
+  }
+  
+  function addRoles() {
+      connection.query ("select * from department", async function(err, results) {
+  
+          const departments = results.map ( (result) => ({
+              name:result.name, 
+              value:result.id
+          }) )
+  
+          const roleData = await inquirer.prompt([
+              {
+                  name: "title",
+                  message: "What is the role title?"
+              },
+              {
+                  name: "salary",
+                  message: "What is the salary?"
+              },
+              {
+                  type: "list",
+                  name: "department_id",
+                  message: "Which Department would you like to place this role?",
+                  choices:departments 
+              }
+          ])
+          connection.query (`insert into role (title, salary, department_id) values('${roleData.title}','${roleData.salary}','${roleData.department_id}' )`, printResults)
+      })
+  }
+  
+  function addEmployees() {
+      connection.query ("select * from role", async function(err, results) {
+          const roles = results.map ( (result) => ({
+              name:result.title, 
+              value:result.id
+          }) )
+  
+          const employeeData = await inquirer.prompt([
+              {
+                  name: "first_name",
+                  message: "What is the first name of the employee"
+              },
+              {
+                  name: "last_name",
+                  message: "What is the last name of the employee"
+              },
+              {
+                  type: "list",
+                  name: "role_id",
+                  message: "What is the employee's role?",
+                  choices:roles 
+              }
+          ])
+          connection.query (`insert into employee (first_name, last_name, role_id) values('${employeeData.first_name}','${employeeData.last_name}','${employeeData.role_id}' )`, printResults)
+      })
+  }
+  
+  function updateEmployees() {
+  
+      connection.query("select * from employee", function (err, employees) {
+  
+          connection.query ("select * from role", async function(err, roles) {
+              const roleChoices = roles.map ( (role) => ({
+                  name:role.title, 
+                  value:role.id
+              }) )
+  
+              const employeeChoices = employees.map ( (employee) => ({
+                  name:employee.first_name + " " + employee.last_name, 
+                  value:employee.id
+              }) )
+  
+              const updateEmployee = await inquirer.prompt([
+                  {
+                      type: "list",
+                      name: "employee_id",
+                      message: "Which employee would you like to udate?",
+                      choices:employeeChoices 
+                  },
+                  {
+                      type: "list",
+                      name: "role_id",
+                      message: "What would you like their new role to be?",
+                      choices:roleChoices 
+                  }
+              ])
+              connection.query (`update employee set role_id=${updateEmployee.role_id} where id=${updateEmployee.employee_id}`, printResults)
+          })
+      })
+  }
+//selct first_name. last_name, titile, salary, name from employee
 //inner join role on viewEmployees.role_id=role.id 
 //inner join department on role.department_id=department.id
 
--- seed.sql
+++ seed.sql
@@ -4,13 +4,13 @@ values ("Sales"), ("Engineering"), ("Finance"), ("Legal");
 
 select * from department;
 
-insert into role (title, slary, department_id)
+insert into role (title, salary, department_id)
 values ("Sales Lead", 100000, 1), ("CTO", 500000, 2), ("Accountant", 120000,3), ("Lawyer", 150000, 4);
 
 select * from role;
 
 insert into employee (first_name, last_name, role_id)
-values ("Chu", "Chu", 1), ("Kobe", "Kobe",2), ("Dan", "Dan", 3). ("Steve", "S", 4);
+values ("Steven", "Universe", 1), ("Ruby", "Sapphire",2), ("Pink", "Diamond", 3), ("Greg", "DaMayo", 4);
 
 select * from employee;
 
